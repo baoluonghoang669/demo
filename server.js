246df845ec000b2d201212a0091f52e42547ff12
hoangbao <baolh@runsystem.net>
2021-05-04 09:02:34 +0700

    push

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get(/.*/, function(req, res) {
     res.sendFile(path.join(__dirname, "/dist/index.html"));
 });
 
-const port = process.env.VUE_APP_PORT || 8080;
+const port = 8080;
 app.listen(port);
 console.log(`app is listening on port ${port}`);

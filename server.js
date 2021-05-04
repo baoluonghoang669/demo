0565257d20898ca86b556154d2f13d631d954c16
hoangbao <baolh@runsystem.net>
2021-05-04 22:05:22 +0700

    deployy

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const serveStatic = require("serve-static");
+// const serveStatic = require("serve-static");
 const path = require("path");
 const app = express();
 
-app.use("/", serveStatic(path.join(__dirname, "/dist")));
+app.use("/", express.static(path.join(__dirname, "/dist")));
 
 const port = 8080;
 app.listen(port);

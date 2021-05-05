a40f9884c8ec592f77145f5f02bc101a65708a42
hoangbao <baolh@runsystem.net>
2021-05-05 15:09:01 +0700

    make it better

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 import interceptors from "./axios.interceptor";
 require("dotenv").config({
-  path: "../../.env",
+    path: "../../.env",
 });
 const http = axios.create({
-  baseURL: process.env.VUE_APP_API_URL,
-  withCredentials: false,
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-  },
-  timeout: 30000, // request timeout
+    baseURL: "https://interior-architecture.herokuapp.com/api/v1/",
+    withCredentials: false,
+    headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+    },
+    timeout: 30000, // request timeout
 });
 
 interceptors.setup(http);
